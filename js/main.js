82ba914791feb3bf480cfa95792587cf7eb97d95
zaenal muttakin <zmuttakin000@gmail.com>
2024-03-08 23:03:54 +0700

    Menambah burger menu

--- js/main.js
+++ js/main.js
@@ -5,3 +5,22 @@ mntoggle.addEventListener('click',function(){
     nav.classList.toggle('menushow');
 })
 
+
+function toggleMenu() {
+  var menu = document.getElementById("menu");
+  var burgerBars = document.querySelectorAll(".burger-menu .bar");
+  
+  if (menu.style.right === "0px") {
+      menu.style.right = "-200px";
+      burgerBars.forEach(bar => {
+          bar.style.transform = "rotate(0)";
+      });
+  } else {
+      menu.style.right = "0px";
+      burgerBars.forEach(bar => {
+          bar.style.transform = "rotate(45deg)";
+      });
+  }
+  menu.classList.toggle('open'); // Toggle the 'open' class
+}
+
